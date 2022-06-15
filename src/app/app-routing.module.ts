5d4184a49376408b0f2b9dbc6038670fff480beb
Jake Kuss <jacob.kuss@edassist.com>
2022-06-15 18:15:55 -0400

    UI Demo components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModalDemoComponent } from "../components/modal-demo/modal-demo.component";
+import { CarouselDemoComponent } from "../components/carousel-demo/carousel-demo.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'modal',
+    component: ModalDemoComponent
+  },
+  {
+    path: 'carousel',
+   component: CarouselDemoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbCarouselModule } from "@ng-bootstrap/ng-bootstrap";
+import { CarouselDemoComponent } from '../components/carousel-demo/carousel-demo.component';
+import { ModalDemoComponent } from "../components/modal-demo/modal-demo.component";
+import { CarouselComponent } from "../ui/carousel/carousel.component";
+import { ModalComponent } from "../ui/modal/modal.component";
+import  { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselDemoComponent,
+    ModalDemoComponent,
+    CarouselComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    NgbCarouselModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
